3a3734168bc6d37b1153a50e9ce63b2f683af401
abhishek <abhishekraibagi81@gmail.com>
2023-06-20 20:21:53 +0530

    add access item form list

---
+++ access_item.py
@@ -0,0 +1,13 @@
+# Accessing through index
+spam1 = [1, 2, 3, [4, 5, 6], 'a', 'b', 'c']
+print(spam1[3])
+
+# Accessing through slicing
+
+spam2 = ['a', 'b', 'c', "d", "e"]
+print(spam2[:3]) # ['a', 'b', 'c']
+
+print(spam2[1:]) # ['b', 'c', 'd', 'e']
+
+print(spam2[1:3]) # ['b', 'c']
+
