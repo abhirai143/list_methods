9872d264ae4d5b199b1f56b1bb141e14394ee85a
abhishek <abhishekraibagi81@gmail.com>
2023-06-23 13:56:05 +0530

    adding other methods

---
+++ other_method.py
@@ -0,0 +1,24 @@
+
+# copy() method -> The copy() method returns a copy of the specified list.
+
+fruits = ["apple", "banana", "cherry"]
+
+x = fruits.copy()
+
+print(x)
+
+# reverse() method -> The reverse() method reverses the sorting order of the elements.
+
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.reverse()
+
+print(fruits)
+
+# sort() method -> The sort() method sorts the list ascending by default.
+
+cars = ['Ford', 'BMW', 'Volvo']
+
+cars.sort()
+
+print(cars)
